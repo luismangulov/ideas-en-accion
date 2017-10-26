0b089271be7001196585c5dd84446d8e77422d7a
luismangulov <luis.m.angulo.v@gmail.com>
2017-10-26 08:59:38 -0500

    Update Response.php

--- ideasaccion/vendor/yiisoft/yii2/web/Response.php
+++ ideasaccion/vendor/yiisoft/yii2/web/Response.php
@@ -378,7 +378,7 @@ class Response extends \yii\base\Response
             /*****
             setcookie($cookie->name, $value, $cookie->expire, $cookie->path, $cookie->domain, $cookie->secure, $cookie->httpOnly);
             *****/
-            setcookie($cookie->name, $value, $cookie->expire, $cookie->path, '/', true, $cookie->httpOnly);
+            setcookie($cookie->name, $value, $cookie->expire, $cookie->path, 'ideasenaccion.perueduca.pe', true, $cookie->httpOnly);
         }
     }
 
